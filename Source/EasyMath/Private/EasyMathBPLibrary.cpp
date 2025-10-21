e80f0ed40c1ee626c3407310fe3680a0de4b12f7
Luogongkai <rvo0428@163.com>
2025-10-21 15:23:49 +0800

    #Update init

---
+++ Source/EasyMath/Private/EasyMathBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EasyMathBPLibrary.h"
+#include "EasyMath.h"
+
+UEasyMathBPLibrary::UEasyMathBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UEasyMathBPLibrary::EasyMathSampleFunction(float Param)
+{
+	return -1;
+}
+
